80abcf3ec418003fca00b49f0f5eaf042abb44c5
Simone Bookman <simonebookman@10-18-147-205.dynapool.wireless.nyu.edu>
2024-09-04 13:20:54 -0400

    added Party and Playlist to models and admin

--- WhosOnAux/admin.py
+++ WhosOnAux/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Party, Playlist
 
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Party)
+admin.site.register(Playlist)
--- WhosOnAux/models.py
+++ WhosOnAux/models.py
@@ -6,9 +6,12 @@ from django.utils import timezone
 # class ClassName(models.Model):
 #    class_name_text = models.FieldType(kwargs=value)
 #    see: https://docs.djangoproject.com/en/5.1/ref/models/fields/#field-types
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
     def __str__(self):
         return self.question_text
 
@@ -20,15 +23,35 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
 
+
 class Party(models.Model):
-    host = models.IntegerField(default=0)
+    host_id = models.IntegerField(default=0)
     party_id = models.IntegerField(default=0)
     playlist_id = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
+    # date = models.DateTimeField("event date")  # TODO: error message, needs default value
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Playlist(models.Model):
+    """
+    TODO: not sure this is the most efficient way to store the playlist
+    This will stor all playlist transactions in one database
+    might be better to create a new database for each party?
+    database can then be destroyed 1 week after party date?
+    """
+    name = models.CharField(max_length=200)       # spotify_id
+    track_name = models.CharField(max_length=200)
+    track_ID = models.CharField(max_length=200)    # spotify_id
+    added_by = models.IntegerField(default=0)      # user_id
+    # added_date = models.DateTimeField("added date") # TODO: error message, needs default value
 
     def __str__(self):
         return self.name
