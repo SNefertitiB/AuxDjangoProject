81d1c52786544e00a3682014e627f0921be26eeb
Simone Bookman <snb331@nyu.edu>
2024-10-17 18:03:08 -0400

    tests for signup + error if email already used

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,117 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
+class ViewsTests(TestCase):
+    def test_valid_signup(self):
+        """
+        Tests signup works with valid form input
+        status code 302 for redirect after log in
+        new User.object is created
+        the new user has the right username
+        the new user has the right email
+        :return: None
+        """
+        username = 'TestUser'
+        email = 'test@fakemail.com'
+        password = 'better_password'
+        valid_form_data = {'username': username,
+                           'email': email,
+                           'password1': password,
+                           'password2': password}
+        response = self.client.post(reverse("signup"), valid_form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(User.objects.count(), 1)
+        new_user = User.objects.first()
+        self.assertEqual(new_user.username, username)
+        self.assertEqual(new_user.email, email)
+
+    def test_signup_username_taken(self):
+        """
+        test user already exists with username
+        verify no new user is created
+        status code 200 (reload sign up page)
+        :return:None
+        """
+        existing_user = User.objects.create(username="Testuser",
+                                            email='something@fakemail.com')
+        username = existing_user.username
+        email = 'test@fakemail.com'
+        password = 'better_password'
+        form_data = {'username': username,
+                     'email': email,
+                     'password1': password,
+                     'password2': password}
+        response = self.client.post(reverse("signup"), form_data)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_email_taken(self):
+        """
+        test user already exists with username
+        verify no new user is created
+        status code 200 (reload sign up page)
+        :return:None
+        """
+        existing_user = User.objects.create(username="Testuser",
+                                            email='something@fakemail.com')
+        username = "A_user_name"
+        email = existing_user.email
+        password = 'better_password'
+        form_data = {'username': username,
+                     'email': email,
+                     'password1': password,
+                     'password2': password}
+        response = self.client.post(reverse("signup"), form_data)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_weak_password(self):
+        """
+        test signup if weak password is used
+        no new user is created
+        page is reloaded
+        :return: None
+        """
+        username = 'TestUser'
+        email = 'test@fakemail.com'
+        password = 'password'
+        form_data = {'username': username,
+                           'email': email,
+                           'password1': password,
+                           'password2': password}
+        response = self.client.post(reverse("signup"), form_data)
+        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_passwords_not_match(self):
+        """
+        test signup if weak password1 != password 2
+        no new user is created
+        page is reloaded
+        :return: None
+        """
+        username = 'TestUser'
+        email = 'test@fakemail.com'
+        password1 = 'good_password'
+        password2 = 'better_password'
+        form_data = {'username': username,
+                           'email': email,
+                           'password1': password1,
+                           'password2': password2}
+        response = self.client.post(reverse("signup"), form_data)
+        self.assertEqual(User.objects.count(), 0)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_logged_in_user(self):
+        """
+        test signup page if user already logged in
+        redirect to home
+        :return: None
+        """
+        user = User.objects.create(username='TestUser')
+        self.client.force_login(user)
+        response = self.client.post(reverse("signup"))
+        self.assertEqual(response.status_code, 302)
--- accounts/views.py
+++ accounts/views.py
@@ -6,33 +6,35 @@ from .forms import UserCreationForm, SignupForm
 from django.contrib.auth import authenticate, login
 from WhosOnAux.views import user_home
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-# class SignUpView(CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy("login")
-#     template_name = "registration/signup.html"
 
 def signup(request):
-    # TODO: error message if username or email has already been used
-    form = SignupForm(request.POST)
     if request.user.is_authenticated:
         return redirect('user_home')
-    else:
+
+    form = SignupForm(request.POST)
+    context = {'form': form}
+
+    if request.method == 'POST':
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            form.save()
-            new_user = authenticate(username=username, password=password)
-            if new_user is not None:
+            email = form.cleaned_data.get('email')
+            if not User.objects.filter(email=email).exists():
+                form.save()
+                new_user = User.objects.get(email=email)
                 messages.success(request, 'Your account has been created successfully.')
                 login(request, new_user)
                 return redirect("user_home")
-
+            else:
+                messages.error(request,
+                               'An account with that email already exists. Please log in or sign up with a different email')
         else:
-            messages.info(request, 'invalid registration details')
-    context = {
-        'form': form,
-    }
+            attempted_username = form.cleaned_data.get('username')
+            if User.objects.filter(username=attempted_username):
+                messages.error(request, 'An account with that username already exists. Please log in or sign up with a different username')
+            messages.error(request, 'invalid registration details')
+
     return render(request, 'registration/signup.html', context)
+
